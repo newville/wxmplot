0f24f1117fee3d7cc464afdfaaff51ee0d1c1e6e
Matt Newville <newville@cars.uchicago.edu>
2011-11-19 11:39:17 -0600

    fixes for doc building

--- publish_docs.sh
+++ publish_docs.sh
@@ -24,12 +24,6 @@ if  [ $? -ne 0 ]  ; then
   exit 
 fi
 
-echo "# Make sure this script is updated!"
-git checkout master publish_docs.sh
-if  [ $? -ne 0 ]  ; then 
-  echo ' failed.'
-  exit 
-fi
 
 tar xzf ../_docs.tgz .
 

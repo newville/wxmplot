225efc69d544364d229bc51179787551445f3695
Matt Newville <newville@cars.uchicago.edu>
2011-11-19 11:33:27 -0600

    add publish_docs.sh

---
+++ publish_docs.sh
@@ -0,0 +1,55 @@
+docbuild='doc/_build'
+
+cd doc 
+echo '# Making docs'
+make all
+cd ../
+
+echo '# Building tarball of docs'
+mkdir _tmpdoc
+cp -pr doc/*.pdf     _tmpdoc/.
+cp -pr doc/_build/html/*   _tmpdoc/.
+cd _tmpdoc
+tar czf ../../_docs.tgz .
+cd ..
+rm -rf _tmpdoc 
+
+# 
+
+echo "# Switching to gh-pages branch"
+git checkout gh-pages
+
+if  [ $? -ne 0 ]  ; then 
+  echo ' failed.'
+  exit 
+fi
+
+echo "# Make sure this script is updated!"
+git checkout master publish_docs.sh
+if  [ $? -ne 0 ]  ; then 
+  echo ' failed.'
+  exit 
+fi
+
+tar xzf ../_docs.tgz .
+
+echo "# commit changes to gh-pages branch"
+git commit -am "updated docs"
+
+if  [ $? -ne 0 ]  ; then 
+  echo ' failed.'
+  exit 
+fi
+
+echo "# Pushing docs to github"
+git push
+
+
+echo "# switch back to master branch"
+git checkout master
+
+if  [ $? -ne 0 ]  ; then 
+  echo ' failed.'
+  exit 
+fi
+

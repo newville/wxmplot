5d15917b694df94e08f3f6767d1837c2b5f8c05d
Matt Newville <newville@cars.uchicago.edu>
2011-11-22 16:20:26 -0600

    improved setup / pkg-info scripts

--- setup.py
+++ setup.py
@@ -10,8 +10,12 @@ setup(
     version = lib.__version__,
     author = 'Matthew Newville',
     author_email = 'newville@cars.uchicago.edu',
+    url = 'http://newville.github.com/wxmplot/',
     license = 'Python',
     description = 'high level wxPython Components for 2D plotting and image display using matplotlib.',
+    classifiers=['Intended Audience :: Science/Research'
+                 'Topic :: Scientific/Engineering',
+                 'Topic :: Scientific/Engineering :: Visualization'],
     package_dir = {'wxmplot': 'lib'},
     packages = ['wxmplot'],
 )

e0c18c43e7e331b1614b573b32dda6e112838526
Matt Newville <newville@cars.uchicago.edu>
2011-11-22 13:30:28 -0600

    fixed image.py example

--- examples/image.py
+++ examples/image.py
@@ -3,8 +3,8 @@ from numpy import exp, random, arange, outer
 from wxmplot import ImageFrame
 
 def gauss2d(x, y, x0, y0, sx, sy):
-    return outer( exp( -(((x-x0)/float(sx))**2)/2),
-                  exp( -(((y-y0)/float(sy))**2)/2) )
+    return outer(exp(-(((y-y0)/float(sy))**2)/2),
+                 exp(-(((x-x0)/float(sx))**2)/2))
 
 if __name__ == '__main__':
     app = wx.PySimpleApp()
@@ -14,14 +14,14 @@ if __name__ == '__main__':
     y = arange(ny)
     ox =  x / 62.
     oy = -2 + y / 97.0
-    dat  = 0.3 * random.random(size=nx*ny).reshape(ny, nx)
-    dat =  dat + 0.25 + (6.0*gauss2d(x, y, 90,   76,  5,  6) +
-                  1.0*gauss2d(x, y, 180, 100,  7,  3) +
-                  1.0*gauss2d(x, y, 175,  98,  3,  7) +
-                  0.5*gauss2d(x, y, 181,  93,  4, 11) +
-                  1.8*gauss2d(x, y, 270, 230, 78, 63) +
-                  0.9*gauss2d(x, y, 240, 265,  8,  3) +
-                  7.0*gauss2d(x, y, 40,  310,  2,  3))
+    dat = 0.2 + (0.3*random.random(size=nx*ny).reshape(ny, nx) +
+                 6.0*gauss2d(x, y, 90,   76,  5,  6) +
+                 1.0*gauss2d(x, y, 180, 100,  7,  3) +
+                 1.0*gauss2d(x, y, 175,  98,  3,  7) +
+                 0.5*gauss2d(x, y, 181,  93,  4, 11) +
+                 1.8*gauss2d(x, y, 270, 230, 78, 63) +
+                 0.9*gauss2d(x, y, 240, 265,  8,  3) +
+                 7.0*gauss2d(x, y, 40,  310,  2,  3) )
 
     frame.display(dat, x=ox, y=oy)
     frame.Show()

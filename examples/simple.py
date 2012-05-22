172535e2783e5b6f7f32fc818fb9ed364388b829
Matt Newville <newville@cars.uchicago.edu>
2012-05-22 15:18:17 -0500

    updated simple example

--- examples/simple.py
+++ examples/simple.py
@@ -10,11 +10,11 @@ x   = numpy.arange(0.0,10.0,0.1)
 y   = numpy.sin(2*x)/(x+2)
 app = wx.PySimpleApp()
 
-pframe = wxmplot.PlotFrame()
+pframe = wxmplot.PlotFrame(output_title='simple')
 pframe.plot(x, y, title='Test Plot',
             xlabel=r'  ${ R \mathrm{(\AA)}}$  ')
 
-pframe.write_message('MPlot PlotFrame example: Try Help->Quick Reference')
+pframe.write_message('WXMPlot PlotFrame example: Try Help->Quick Reference')
 pframe.Show()
 #
 app.MainLoop()

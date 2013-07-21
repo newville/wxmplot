ff68104ebcd243de195d397940db8af7a3544853
Matt Newville <newville@cars.uchicago.edu>
2013-07-21 10:28:57 -0500

    fix basepanel lasso to allow for cmap to be a dict

--- lib/basepanel.py
+++ lib/basepanel.py
@@ -536,9 +536,14 @@ class BasePanel(wx.Panel):
             # set lasso color
             color='goldenrod'
             cmap = getattr(self.conf, 'cmap', None)
-            if cmap is not None:
-                rgb = (int(i*255)^255 for i in cmap._lut[0][:3])
-                color = '#%02x%02x%02x' % tuple(rgb)
+            if isinstance(cmap, dict):
+                cmap = cmap['int']
+            try:
+                if cmap is not None:
+                    rgb = (int(i*255)^255 for i in cmap._lut[0][:3])
+                    color = '#%02x%02x%02x' % tuple(rgb)
+            except:
+                pass
             self.lasso = Lasso(event.inaxes, (event.xdata, event.ydata),
                                self.lassoHandler)
             self.lasso.line.set_color(color)

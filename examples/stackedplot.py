4c98a480aabc277aec60a4d8c8dce391ba15727f
Matthew Newville <newville@cars.uchicago.edu>
2016-12-14 16:46:31 -0600

    add stacked plot example

---
+++ examples/stackedplot.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+#
+# Stacked Plot shows two related plots stacked top and bottom that are
+# expected to share an X axis.
+
+# The upper panel is a standard PlotPanel. The bottom panel is compressed
+# in height, and follows the X-range when zooming the top panel, and does
+# not respond to most other mouse events.
+
+import wx
+import numpy
+from wxmplot import StackedPlotFrame
+
+x = numpy.arange(0.0, 10.0, 0.1)
+noise = numpy.random.normal(size=len(x), scale=0.06)
+
+y1 = numpy.sin(2*x)/(x+2)
+y2 = y1 + noise
+
+app = wx.App()
+
+pframe = StackedPlotFrame(title='Stacked Example', ratio=3.0)
+
+pframe.plot(x, y2, label='data(fake)', ylabel='signal',xlabel='x')
+
+pframe.oplot(x, y1, label='simple theory', show_legend=True)
+pframe.plot(x, noise, panel='bottom', ylabel='residual')
+
+
+pframe.Show()
+pframe.Raise()
+
+app.MainLoop()

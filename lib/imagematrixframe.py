41d0baae0ffb8f74a87166fd7edbfe1b05607468
Matthew Newville <newville@cars.uchicago.edu>
2016-12-16 14:18:58 -0600

    many feature improvements, mostly working

--- lib/imagematrixframe.py
+++ lib/imagematrixframe.py
@@ -22,27 +22,31 @@ from functools import partial
 from .baseframe  import BaseFrame
 from .plotpanel  import PlotPanel
 from .imagepanel import ImagePanel
-from .imageframe import ColorMapPanel
+from .imageframe import ColorMapPanel, AutoContrastDialog
 from .colors import rgb2hex
 from .utils import LabelEntry, MenuItem, pack
 
+COLORMAPS = ('blue', 'red', 'green', 'magenta', 'cyan', 'yellow')
+
+def color_complements(color):
+    colors = list(COLORMAPS)
+    colors.remove(color)
+    return colors
 
 class ImageMatrixFrame(BaseFrame):
     """
     wx.Frame, with 3 ImagePanels and correlation plot for 2 map arrays
     """
-    def __init__(self, parent=None, size=(900,600),
-                 subtitles=None, **kws):
+    def __init__(self, parent=None, size=(900,600), cursor_callback=None, **kws):
 
         self.sel_mask = None
-
+        self.xdata = None
+        self.ydata = None
+        self.cursor_callback = cursor_callback
         BaseFrame.__init__(self, parent=parent,
                            title  = 'Image Matrix', size=size, **kws)
-        self.cmap_panels= {}
-        self.subtitles = {}
-        if subtitles is not None:
-            self.subtitles = subtitles
 
+        self.cmap_panels= {}
         sbar = self.CreateStatusBar(2, wx.CAPTION)
         sfont = sbar.GetFont()
         sfont.SetWeight(wx.BOLD)
@@ -52,10 +56,7 @@ class ImageMatrixFrame(BaseFrame):
         self.SetStatusWidths([-2, -1])
         self.SetStatusText('', 0)
 
-        self.optional_menus = []
-
         self.bgcol = rgb2hex(self.GetBackgroundColour()[:3])
-
 
         self.SetBackgroundColour('#F8F8F4')
 
@@ -64,6 +65,8 @@ class ImageMatrixFrame(BaseFrame):
 
         conf_panel = wx.Panel(splitter)
         main_panel = wx.Panel(splitter)
+
+        self.config_panel = conf_panel
 
         # main panel
         self.plot_panel = PlotPanel(main_panel, size=(400, 325))
@@ -88,22 +91,25 @@ class ImageMatrixFrame(BaseFrame):
 
         ir = 0
         self.wids = {}
-        cmap_colors = ('blue', 'red', 'green', 'magenta', 'cyan', 'yellow')
         self.cmap_panels = [None, None]
         csizer = wx.BoxSizer(wx.VERTICAL)
         for i, imgpanel in enumerate((self.img1_panel, self.img2_panel)):
             self.cmap_panels[i] =  ColorMapPanel(conf_panel, imgpanel,
                                                  title='Map %i: ' % (i+1),
                                                  color=0,
-                                                 default=cmap_colors[i],
-                                                 colormap_list=cmap_colors)
+                                                 default=COLORMAPS[i],
+                                                 colormap_list=COLORMAPS,
+                                                 cmap_callback=partial(self.onColorMap, index=i))
 
             csizer.Add(self.cmap_panels[i], 0, lsty, 2)
             csizer.Add(wx.StaticLine(conf_panel, size=(100, 2),
                                     style=wx.LI_HORIZONTAL), 0, lsty, 2)
 
-        pack(conf_panel, csizer)
 
+        cust = self.CustomConfig(conf_panel)
+        if cust is not None:
+            sizer.Add(cust, 0, lsty, 1)
+        pack(conf_panel, csizer)
 
         for name, panel in (('corplot', self.plot_panel),
                             ('map1',    self.img1_panel),
@@ -113,12 +119,12 @@ class ImageMatrixFrame(BaseFrame):
             panel.report_leftdown = partial(self.report_leftdown, name=name)
             panel.messenger = self.write_message
 
-        sizer = wx.GridSizer(2, 2, 3, 3)
+        sizer = wx.GridSizer(2, 2, 2, 2)
         lsty |= wx.GROW|wx.ALL|wx.EXPAND|wx.ALIGN_CENTER|wx.ALIGN_CENTER_VERTICAL
-        sizer.Add(self.img1_panel, 1, lsty, 5)
-        sizer.Add(self.dual_panel, 1, lsty, 5)
-        sizer.Add(self.plot_panel, 1, lsty, 5)
-        sizer.Add(self.img2_panel, 1, lsty, 5)
+        sizer.Add(self.img1_panel, 1, lsty, 2)
+        sizer.Add(self.dual_panel, 1, lsty, 2)
+        sizer.Add(self.plot_panel, 1, lsty, 2)
+        sizer.Add(self.img2_panel, 1, lsty, 2)
 
         pack(main_panel, sizer)
         splitter.SplitVertically(conf_panel, main_panel, 1)
@@ -129,14 +135,47 @@ class ImageMatrixFrame(BaseFrame):
         sizer.Add(splitter, 1, wx.GROW|wx.ALL, 5)
         pack(self, sizer)
 
+        self.dualimage_needs_update = False
+        self.dualimage_timer = wx.Timer(self)
+        self.Bind(wx.EVT_TIMER, self.onDualImageTimer, self.dualimage_timer)
+        self.dualimage_timer.Start(150)
+
+
+    def CustomConfig(self, parent):
+        """
+        override to add custom config panel items to bottom of config panel
+        """
+        pass
+
+    def onColorMap(self, name=None, index=None):
+        colors = color_complements(name)
+        opanel = self.cmap_panels[0]
+        if index == 0:
+            opanel = self.cmap_panels[1]
+
+        c1 = opanel.cmap_choice.GetStringSelection()
+        opanel.cmap_choice.Clear()
+        for c in colors:
+            opanel.cmap_choice.Append(c)
+        if c1 in colors:
+            opanel.cmap_choice.SetStringSelection(c1)
+        else:
+            opanel.cmap_choice.SetStringSelection(colors[0])
+            opanel.set_colormap(name=colors[0])
+            opanel.imgpanel.redraw()
+
+
     def unzoom(self, event=None):
+        self.xzoom = slice(0, self.map1.shape[1]+1)
+        self.yzoom = slice(0, self.map1.shape[0]+1)
+
         self.update_scatterplot(self.map1, self.map2)
+        self.plot_panel.unzoom_all()
         for p in self.imgpanels:
             p.unzoom_all()
-        self.plot_panel.unzoom_all()
 
     def save_figure(self, event=None):
-        print(" Save Figure ??")
+        print(" Save Figure not implemented yet")
 
 
     def BuildMenu(self):
@@ -148,11 +187,17 @@ class ImageMatrixFrame(BaseFrame):
 
         MenuItem(self, mfile, "E&xit\tCtrl+Q", "Exit", self.onExit)
 
-        # options menu
         mview =  wx.Menu()
         MenuItem(self, mview, "Zoom Out\tCtrl+Z",
                  "Zoom out to full data range",
                  self.unzoom)
+        MenuItem(self, mview, 'Toggle Contrast Enhancement\tCtrl+E',
+                 'Toggle contrast between auto-scale and full-scale',
+                 self.onEnhanceContrast, kind=wx.ITEM_CHECK)
+
+        MenuItem(self, mview, 'Set Auto-Contrast Level',
+                 'Set auto-contrast scale',
+                 self.onContrastConfig)
 
         mbar = wx.MenuBar()
         mbar.Append(mfile, 'File')
@@ -161,47 +206,94 @@ class ImageMatrixFrame(BaseFrame):
         self.SetMenuBar(mbar)
         self.Bind(wx.EVT_CLOSE,self.onExit)
 
-    def report_leftdown(self, event=None, name=None):
-        print("report leftdown, ", name, event)
+    def onEnhanceContrast(self, event=None):
+        """change image contrast, using scikit-image exposure routines"""
+        for ipanel in self.imgpanels:
+            ipanel.conf.auto_contrast = event.IsChecked()
+            ipanel.redraw()
+        self.set_contrast_levels()
 
+
+    def onContrastConfig(self, event=None):
+        dlg = AutoContrastDialog(parent=self, conf=self.img1_panel.conf)
+        dlg.CenterOnScreen()
+        val = dlg.ShowModal()
+        if val == wx.ID_OK:
+            for ipanel in self.imgpanels:
+                ipanel.conf.auto_contrast_level = self.img1_panel.conf.auto_contrast_level
+        dlg.Destroy()
+        self.set_contrast_levels()
+
+    def report_leftdown(self,event=None, name=''):
+        if event is None:
+            return
+        if event.xdata is None or event.ydata is None:
+            return
+        ix, iy = int(round(event.xdata)), int(round(event.ydata))
+        if (ix >= 0 and ix < self.map1.shape[1] and
+            iy >= 0 and iy < self.map1.shape[0]):
+            pos = ''
+            if self.xdata is not None:
+                pos = ' %s=%.4g,' % (self.xlab, self.xdata[ix])
+            if self.ydata is not None:
+                pos = '%s %s=%.4g,' % (pos, self.ylab, self.ydata[iy])
+
+            d1, d2 = (self.map1[iy, ix], self.map2[iy, ix])
+            msg = "Pixel [%i, %i],%s Map1=%.4g, Map2=%.4g" % (ix, iy, pos, d1, d2)
+            self.write_message(msg, panel=0)
+
+            if callable(self.cursor_callback):
+                self.cursor_callback(x=event.xdata, y=event.ydata)
 
     def on_corplot_lasso(self, data=None, selected=None, mask=None):
         self.sel_mask = mask
         try:
-            self.sel_mask.shape = self.zoom_map1.shape
-            self.update_dualimage()
+            mask.shape = self.zoom_map1.shape
+            self.sel_mask = 0 * self.map1
+            self.sel_mask[self.yzoom, self.xzoom] = mask
+            self.dualimage_needs_update = True
         except:
             pass
 
     def on_imagezoom(self, event=None, wid=0, limits=None):
         if wid in [w.GetId() for w in self.imgpanels]:
-            lims = [int(round(x)) for x in limits]
+            lims = [int(x) for x in limits]
             for ipanel in self.imgpanels:
                 ax = ipanel.fig.axes[0]
                 axlims = {ax: lims}
                 ipanel.conf.zoom_lims.append(axlims)
                 ipanel.set_viewlimits()
 
-        m1  = self.map1[lims[2]:lims[3], lims[0]:lims[1]]
-        m2  = self.map2[lims[2]:lims[3], lims[0]:lims[1]]
+        m1 = self.map1[lims[2]:lims[3], lims[0]:lims[1]]
+        m2 = self.map2[lims[2]:lims[3], lims[0]:lims[1]]
+        self.xzoom = slice(lims[0], lims[1])
+        self.yzoom = slice(lims[2], lims[3])
         self.update_scatterplot(m1, m2)
 
     def on_imageredraw(self, event=None, wid=0):
         if wid in (self.img1_panel.GetId(), self.img2_panel.GetId()):
-            self.update_dualimage()
+            self.dualimage_needs_update = True
 
-    def display(self, map1, map2, title=None, x=None, y=None, xoff=None, yoff=None,
-                subtitles=None, xrmfile=None, det=None):
-
+    def display(self, map1, map2, title=None, xlabel=None, ylabel=None,
+                x=None, y=None, xoff=None, yoff=None, **kws):
         self.map1 = map1
         self.map2 = map2
-        self.img1_panel.display(map1)
-        self.img2_panel.display(map2)
+        self.xdata = x
+        self.ydata = y
+        self.xzoom = slice(0, map1.shape[1]+1)
+        self.yzoom = slice(0, map1.shape[0]+1)
+
+        self.xlabel = xlabel
+        self.ylabel = ylabel
+        self.img1_panel.display(map1, x=x, y=y)
+        self.img2_panel.display(map2, x=x, y=y)
+
+        self.cmap_panels[0].title.SetLabel(xlabel)
+        self.cmap_panels[1].title.SetLabel(ylabel)
         self.update_scatterplot(map1, map2)
 
         self.set_contrast_levels()
-        self.update_dualimage()
-
+        self.dualimage_needs_update = True
 
     def update_scatterplot(self, x, y):
         self.zoom_map1 = x
@@ -213,16 +305,12 @@ class ImageMatrixFrame(BaseFrame):
         ymax = max(_y) * 1.2
         ymin = min(_y) * 0.8
         self.plot_panel.clear()
-        self.plot_panel.scatterplot(_x, _y,
+        self.plot_panel.conf.labelfont.set_size(7)
+        self.plot_panel.scatterplot(_x, _y, size=3,
                                     min=xmin, xmax=xmax,
                                     ymin=ymin, ymax=ymax,
+                                    xlabel=self.xlabel, ylabel=self.ylabel,
                                     callback=self.on_corplot_lasso)
-
-    def onEnhanceContrast(self, event=None):
-        """change image contrast, using scikit-image exposure routines"""
-        self.panel.conf.auto_contrast = event.IsChecked()
-        self.set_contrast_levels()
-        self.panel.redraw()
 
     def set_contrast_levels(self):
         """enhance contrast levels, or use full data range
@@ -252,20 +340,22 @@ class ImageMatrixFrame(BaseFrame):
             cmap_panel.redraw_cmap()
             img_panel.redraw()
 
+    def onDualImageTimer(self, event=None):
+        if not self.dualimage_needs_update:
+            return
 
-    def update_dualimage(self):
+        self.dualimage_needs_update = False
         try:
             col1 = self.img1_panel.conf.cmap[0].name
-            dat1 = self.zoom_map1
+            dat1 = self.map1
             conf = self.img1_panel.conf
             ilo1 = float(conf.int_lo[0])
             ihi1 = float(conf.int_hi[0])
             mlo1 = float(conf.cmap_lo[0])/(1.0*conf.cmap_range)
             mhi1 = float(conf.cmap_hi[0])/(1.0*conf.cmap_range)
 
-
             col2 = self.img2_panel.conf.cmap[0].name
-            dat2 = self.zoom_map2
+            dat2 = self.map2
             conf = self.img2_panel.conf
             ilo2 = float(conf.int_lo[0])
             ihi2 = float(conf.int_hi[0])
@@ -337,5 +427,19 @@ class ImageMatrixFrame(BaseFrame):
                 img[:, :, 1] += dimg
                 img[:, :, 2] += dimg
 
-        self.dual_panel.display(img)
+        dp = self.dual_panel
+        try:
+            dp.conf.image.set_data(img)
+        except:
+            dp.conf.data = img
+        im1_zoom = self.img1_panel.conf.zoom_lims[-1]
+        if im1_zoom is not None:
+            dp.conf.zoom_lims.append({dp.axes:im1_zoom.values()[0]})
+            dp.set_viewlimits()
+            if self.sel_mask is not None:
+                dp.conf.image = dp.axes.imshow(img, cmap=dp.conf.cmap[0],
+                                               interpolation=dp.conf.interp)
+            dp.canvas.draw()
+        else:
+            self.dual_panel.display(img)
         self.sel_mask = None

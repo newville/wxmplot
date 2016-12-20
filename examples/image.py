945185168105da682dd39e08e64dfd20f58c74e4
Matt Newville <newville@cars.uchicago.edu>
2016-12-20 10:54:03 -0600

    update image example, simpler script, better image

--- examples/image.py
+++ examples/image.py
@@ -1,31 +1,17 @@
 import sys
-
 import wx
-
-from numpy import exp, random, arange, outer
+import numpy as np
 from wxmplot import ImageFrame
 
-def gauss2d(x, y, x0, y0, sx, sy):
-    return outer(exp(-(((y-y0)/float(sy))**2)/2),
-                 exp(-(((x-x0)/float(sx))**2)/2))
+y, x = np.mgrid[-5:5:101j, -4:6:101j]
+dat = np.sin(x*x/3.0 + y*y)/(1 + (x+y)*(x+y))
 
-ny, nx = 350, 400
-x = arange(nx)
-y = arange(ny)
-ox =  x / 62.
-oy = -2 + y / 97.0
-dat = 0.2 + (0.3*random.random(size=nx*ny).reshape(ny, nx) +
-             6.0*gauss2d(x, y, 90,   76,  5,  6) +
-             1.0*gauss2d(x, y, 180, 100,  7,  3) +
-             1.0*gauss2d(x, y, 175,  98,  3,  7) +
-             0.5*gauss2d(x, y, 181,  93,  4, 11) +
-             1.8*gauss2d(x, y, 270, 230, 78, 63) +
-             0.9*gauss2d(x, y, 240, 265,  8,  3) +
-             7.0*gauss2d(x, y, 40,  310,  2,  3) )
+x0 = x[0,:]
+y0 = y[:,0]
 
 
 app = wx.App()
 frame = ImageFrame(mode='intensity')
-frame.display(dat, x=ox, y=oy)
+frame.display(dat, x=x0, y=y0)
 frame.Show()
 app.MainLoop()

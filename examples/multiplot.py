1c1e998f3b611c47e288e2a1da0043d70c77e75c
Matthew Newville <newville@cars.uchicago.edu>
2017-04-08 14:27:24 -0500

    update multiplot example (size is in pixels, not inches)

--- examples/multiplot.py
+++ examples/multiplot.py
@@ -6,24 +6,27 @@ import wx
 import wxmplot
 import numpy
 
-x   = numpy.arange(0.0,10.0,0.1)
-y1  = numpy.sin(2*x)/(x+2)
+x   = numpy.linspace(0.0, 10.0, 201)
+y1  = numpy.sin(3*x)/(x+2)
 y2  = numpy.cos(2*x)*numpy.sin(2*x)
 y3  = numpy.cos(2*x) + x/3
 y4  = numpy.cos(2*x)*numpy.exp(-x/10.)
+y5  = y4 + y2
+y6  = 10*y1 +  y3
 app = wx.App()
 
-pframe = wxmplot.MultiPlotFrame(rows=2,cols=3,panelsize=(2.5,1.75))
+pframe = wxmplot.MultiPlotFrame(rows=2, cols=3, panelsize=(350, 275))
 
 
-pframe.plot(x,y1,panel=(0,0))
-pframe.plot(x,y2,panel=(0,1))
-pframe.plot(x,y3,panel=(1,1))
-pframe.plot(x,y4,panel=(1,2))
-pframe.set_title('Test Plot',panel=(0,0))
+pframe.plot(x, y1, panel=(0, 0), labelfontsize=6)
+pframe.plot(x, y2, panel=(0, 1), color='red',  labelfontsize=6)
+pframe.plot(x, y3, panel=(0, 2), color='black', labelfontsize=5)
+pframe.plot(x, y4, panel=(1, 0), fullbox=False)
+pframe.plot(x, y5, panel=(1, 1), show_grid=False)
+pframe.plot(x, y6, panel=(1, 2))
+pframe.set_title('Test Plot', panel=(0, 0))
 pframe.set_title('Plot2',panel=(1,0))
 pframe.set_xlabel(r' $ R  (\AA) $ ')
-pframe.write_message('MPlot PlotFrame example: Try Help->Quick Reference')
 pframe.Show()
 pframe.Raise()
 

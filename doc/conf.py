4a625fbd512dd29641a4bffdeb5313c6139bbe71
Matthew Newville <newville@cars.uchicago.edu>
2018-02-03 11:27:30 -0600

    doc: remove smartypants reference

--- doc/conf.py
+++ doc/conf.py
@@ -134,10 +134,6 @@ html_static_path = ['_static']
 # using the given strftime format.
 #html_last_updated_fmt = '%b %d, %Y'
 
-# If true, SmartyPants will be used to convert quotes and dashes to
-# typographically correct entities.
-html_use_smartypants = False # True
-
 # Custom sidebar templates, maps document names to template names.
 html_sidebars = {'index': ['indexsidebar.html','searchbox.html']}
 

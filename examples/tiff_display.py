f11ead32fcb7090536790f678d44b297d871bebd
Matthew Newville <newville@cars.uchicago.edu>
2018-02-03 21:39:48 -0600

    update tiff example

--- examples/tiff_display.py
+++ examples/tiff_display.py
@@ -3,10 +3,10 @@ import wx
 from tifffile import imread
 from wxmplot import ImageFrame
 
-img = imread('Pilatus.tiff')
+img = imread('ceo2.tiff')
 
 app = wx.App()
 frame = ImageFrame()
-frame.display(img)
+frame.display(img, auto_contrast=True, colormap='plasma')
 frame.Show()
 app.MainLoop()

fa4eadd436500dc3852df6128f533317668bc86d
Matthew Newville <newville@cars.uchicago.edu>
2019-08-18 16:04:34 -0500

    update more examples to use wxmplot.interactive

--- examples/plot_fromdatafile.py
+++ examples/plot_fromdatafile.py
@@ -1,22 +1,14 @@
 #!/usr/bin/python
 
-from wxmplot import PlotApp
+from os import path
+import numpy as np
+from wxmplot.interactive import plot, wxloop
 
-f = open('xafs.dat','r')
-x = []
-y = []
-titles = []
-for i in f.readlines():
-    i =  i[:-1].strip()
-    if i.startswith('#'):
-        titles.append(i)
-    else:
-        j = i.split()
-        x.append(float(j[0]))
-        y.append(float(j[1]))
-f.close()
-app = PlotApp()
-app.plot(x,y,xlabel='E (eV)', label='As K', marker='+',linewidth=1)
-app.set_title('Test Plot')
-app.write_message('MPlot PlotFrame example: Try Help->Quick Reference')
-app.run()
+thisdir, _ = path.split(__file__)
+dat = np.loadtxt(path.join(thisdir, 'xafs.dat'))
+
+x = dat[:, 0]
+y = dat[:, 1]
+
+plot(x, y, xlabel='E (eV)', label='As K edge', title='Test Plot')
+wxloop()
--- examples/simple.py
+++ examples/simple.py
@@ -1,22 +1,10 @@
 #!/usr/bin/python
-#
-# simple example of wxMPlot
-import sys
-import wx
-import numpy
-import wxmplot
 
-x   = numpy.arange(0.0,10.0,0.1)
-y   = numpy.sin(2*x)/(x+2)
-app = wx.App()
+import numpy as np
+from  wxmplot.interactive import plot, wxloop
 
-pframe = wxmplot.PlotFrame(output_title='simple')
-pframe.plot(x, y, title='Test Plot',
-            xlabel=r'  ${ R \mathrm{(\AA)}}$  ')
+x = np.arange(0.0,10.0,0.1)
+y = np.sin(2*x)/(x+2)
 
-pframe.write_message('WXMPlot PlotFrame example: Try Help->Quick Reference')
-pframe.Show()
-#
-app.MainLoop()
-
-
+plot(x, y, title='Test Plot', xlabel=r'${ R \mathrm{(\AA)}}$')
+wxloop()

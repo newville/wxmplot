aba727ffa5fbbd1892ac2e54b3742f765a76a37c
Matthew Newville <newville@cars.uchicago.edu>
2019-08-18 15:40:16 -0500

    updating some examples to use wxmplot.interactive

--- examples/dislin_comp.py
+++ examples/dislin_comp.py
@@ -4,19 +4,16 @@
 #   http://www.mps.mpg.de/dislin/exa_curv.html
 #   http://www.mps.mpg.de/dislin/exa_python.html#section_1
 
-from wxmplot import PlotApp
 import numpy as np
+from wxmplot.interactive import plot, wxloop
 
 x  = 3.6*np.arange(101)
 y1 = np.cos(np.pi*x/180)
 y2 = np.sin(np.pi*x/180)
 
-app = PlotApp()
-app.plot(x, y1, color='red',
-         xlabel='x', ylabel='y',
-         title='DISLIN Comparison\nsin(x) and cos(x)',
-         xmin=0, xmax=360.0)
-app.oplot(x, y2, color='green3', marker='+')
+plot(x, y1, title='DISLIN Comparison\nsin(x) and cos(x)',
+     color='red', xlabel='x', ylabel='y')
 
+plot(x, y2, color='green3', marker='+')
 
-app.run()
+wxloop()

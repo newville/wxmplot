2f3684c414eea60fa743714f3a9c030645141cf6
Matthew Newville <newville@cars.uchicago.edu>
2019-08-19 14:01:18 -0500

    update comparison script

---
+++ examples/compare_dislin_mpl.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+#
+# compare basic wxmplot plotting to DISLIN and matplotlib,
+# inspired from example at
+#     https://www2.mps.mpg.de/dislin/exa_curv.html
+#
+
+# make data
+import numpy as np
+x  = 3.6*np.arange(101)
+y1 = np.cos(np.pi*x/180)
+y2 = np.sin(np.pi*x/180)
+
+# dislin:
+#   See script at:
+#   https://www2.mps.mpg.de/dislin/exa_python.html#section_1
+
+# matplotlib/pyplot:
+mpl_script = """
+import  matplotlib.pyplot as plt
+plt.plot(x, y1, '-', color='r')
+plt.plot(x, y2, '-+', color='g')
+plt.title('DISLIN Comparison\nsin(x) and cos(x)')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.show()
+"""
+
+# wxmplot:
+from wxmplot.interactive import plot, wxloop
+plot(x, y1, color='red', xlabel='x', ylabel='y',
+     title='DISLIN Comparison\nsin(x) and cos(x)')
+plot(x, y2, color='green3', marker='+')
+wxloop()

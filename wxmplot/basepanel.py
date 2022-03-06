ea019d5d3656c6901f10fb462584a9c7f4f161f3
Matthew Newville <newville@cars.uchicago.edu>
2022-03-06 14:22:05 -0600

    need RenderWx

--- wxmplot/basepanel.py
+++ wxmplot/basepanel.py
@@ -14,6 +14,7 @@ import numpy as np
 import matplotlib
 from matplotlib.widgets import Lasso
 from matplotlib import dates
+from matplotlib.backends.backend_wx import RendererWx
 
 from .utils import Printer, MenuItem
 

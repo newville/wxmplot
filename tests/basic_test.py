216c087d9231348edb53cc354dfff8bc02c06269
Matthew Newville <newville@cars.uchicago.edu>
2023-03-26 18:37:13 -0500

    basic import test includes wxmplot.interactive

--- tests/basic_test.py
+++ tests/basic_test.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python
-# simple wxmplot example
+# simple wxmplot import
 
-from numpy import linspace, sin, cos, random
+import numpy
+import matplotlib
 
 def test_import():
     success = False
     try:
         import wxmplot
+        success = True
+    except:
+        pass
+    assert(success)
+
+
+def test_interactive_import():
+    success = False
+    try:
+        import wxmplot.interactive as wi
         success = True
     except:
         pass

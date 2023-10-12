fe707aa5560e680d1bce359fca943b1cb68be380
Matthew Newville <newville@cars.uchicago.edu>
2023-10-12 16:26:04 -0500

    add examples using set_data_generator() to update plotted data from generator or function

---
+++ examples/scope_mode_generator.py
@@ -0,0 +1,28 @@
+import numpy as np
+from wxmplot.interactive import plot, set_data_generator
+
+npts = 501
+x = np.linspace(0, 50, npts)
+
+# generator of datasets
+datasets = ((np.cos(1.3*x) + np.sin(0.8*(x+nx/7)),
+             np.cos(1.1*x) - np.sin(0.6*(x+nx/43)))
+            for nx in range(npts))
+
+def more_data():
+    """yield next pair of datasets from data generator"""
+    while True:
+        try:
+            ds = next(datasets)
+            yield [(x, ds[0]), (x, ds[1])]
+        except StopIteration:
+            break
+
+
+# set up an initial plot
+plot(x, np.zeros(len(x)))
+
+# now set data generator and wait
+set_data_generator(more_data,  polltime=30)
+
+print("consuming data from generator...")

a9f1aad2a4d71eeda48e6a9c2e604f2c4a1d3c86
Matthew Newville <newville@cars.uchicago.edu>
2025-01-19 16:39:52 -0600

    document setting contrast level

--- examples/pilatus_display.py
+++ examples/pilatus_display.py
@@ -9,6 +9,6 @@ dat = imread('Pilatus.tiff')
 
 app = wx.App()
 frame = ImageFrame()
-frame.display(dat)
+frame.display(dat, contrast_level='0.005', auto_contrast=False)
 frame.Show()
 app.MainLoop()

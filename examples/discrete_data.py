3905d16873eab2522c92aeffb7ec2a501e25cabe
Matthew Newville <newville@cars.uchicago.edu>
2025-01-18 19:49:42 -0600

    add example with discrete data

---
+++ examples/discrete_data.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+"""
+example setting yticks for categorical data
+"""
+import numpy as np
+from wxmplot import PlotApp
+
+x = np.arange(100)
+y = (x * 0.06).astype(int)
+
+y[84:86] -= 1
+y[94:] -= 1
+y[67:69] += 1
+y[43:45] += 1
+
+app = PlotApp()
+app.plot(x, y, xlabel='t (s)', ylabel='Y label', title='discrete y data',
+           drawstyle='steps-post')
+
+app.frame.panel.set_ytick_labels({0: 'zero', 1: 'one', 2: 'two',
+                                  3: 'three', 4: 'four', 5: 'five', 6: 'six'})
+
+app.run()

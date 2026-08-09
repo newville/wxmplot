bb23702e8d55a50452dc722de6db921762942571
Matt Newville <newville@cars.uchicago.edu>
2026-08-08 20:20:15 -0500

    simple example includes latex, newlines, unicode text

--- examples/simple.py
+++ examples/simple.py
@@ -3,4 +3,6 @@ import numpy as np
 import wxmplot.interactive as wi
 
 x = np.linspace(0.0, 20.0, 201)
-wi.plot(x, np.sin(x)/(x+1), ylabel='response',  xlabel='T (sec)')
+wi.plot(x, np.sin(x)/(x+1),
+        title='Titles can include latex: $\\chi^2/\\epsilon $ \n and newlines',
+        ylabel='unicde can also be used [\u03c7] (\u212B)',  xlabel='T (sec)')
